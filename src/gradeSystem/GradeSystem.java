4a5b8dbfdfd2d5a95d440985e926e83f4189c25f
Jhih-Sheng, Jhang <jasonchang117@gmail.com>
2017-03-28 19:45:55 +0800

    finish grade class and gradeSystem is now sorted

--- src/gradeSystem/GradeSystem.java
+++ src/gradeSystem/GradeSystem.java
@@ -1,32 +1,80 @@
 package gradeSystem;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+
 public class GradeSystem {
 	private double [] weights;
+	private LinkedList<Grades> gradeList;
 	
-	void GradeSystem()
-	{
+	GradeSystem() throws IOException{
+		this.gradeList = new LinkedList<Grades>();
 		this.weights = new double[] {0.1, 0.1, 0.1, 0.3, 0.4};  // lab_1, lab_2, lab_3, midterm_exam, final_exam
+		Reader reader = new InputStreamReader(new FileInputStream("src/data/gradeinput.txt"), "utf-8");
+		BufferedReader br = new BufferedReader(reader);
 		
+		while( br.ready() ){
+			String [] input;
+			input = br.readLine().split(" ");
+			Grades grade = null;
+			grade = new Grades(input[0], input[1], Integer.parseInt(input[2]), Integer.parseInt(input[3]), Integer.parseInt(input[4]), Integer.parseInt(input[5]), Integer.parseInt(input[6]));
+			grade.calculateTotalGrade(weights);
+			gradeList.add(grade);
+		}
 	}
 	
 	public boolean containsID(String studentID)
 	{
-		
-		return true;
+		if(this.gradeList.contains(studentID))
+			return true;
+		else
+			return false;
 	}
 	
 	public void showGrade(String studentID)
 	{
-		
+		for(int i=0;;i++){
+			if(this.gradeList.get(i).getStudentID().equals(studentID)){
+				this.gradeList.get(i).getScores();		// return a integer score array
+			}
+		}
 	}
 	
 	public void showRank(String studentID)
 	{
-		
+		Collections.sort(this.gradeList, new Comparator<Grades>(){
+			@Override
+			public int compare(Grades g1, Grades g2) {
+				if(g1.getTotalGrade() > g2.getTotalGrade())
+					return 1;
+				else
+					return -1;
+			}
+		});
 	}
 	
-	public void updateWeights()
+	public void updateWeights(double [] newWeight)
 	{
-		
+		for(int i=0;i<5;i++){
+			this.weights[i] = newWeight[i];
+		}
+	}
+	
+	/**
+	 * 
+	 * 
+	 * 
+	 * 
+	 * This main function is just for testing
+	 */
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		GradeSystem gradeSystem = new GradeSystem();
 	}
 }
--- src/gradeSystem/Grades.java
+++ src/gradeSystem/Grades.java
@@ -2,18 +2,42 @@ package gradeSystem;
 
 public class Grades {
 	private int [] scores;
-	private String name;
 	private String studentID;
-	private double totalGrade;
+	private String name;
+	private int totalGrade;
 	
-	void Grades()
+	Grades(String ID, String name, int lab_1, int lab_2, int lab_3, int midterm_exam, int final_exam)
 	{
-		
+		this.scores = new int[] {lab_1, lab_2, lab_3, midterm_exam, final_exam};
+		this.studentID = ID;
+		this.name = name;
+		this.totalGrade = 0;
 	}
 	
 	public void calculateTotalGrade(double [] weights)
 	{
+		double tempScore = 0.0;
+		for(int i=0;i<5;i++)
+			tempScore += this.scores[i]*weights[i];
 		
+		if(tempScore*10%10 < 5)
+			this.totalGrade = (int)tempScore;
+		else
+			this.totalGrade = (int)(tempScore*10 + (10 - tempScore*10%10))/10;
 	}
 	
+	public double getTotalGrade()
+	{
+		return this.totalGrade;
+	}
+	
+	public String getStudentID()
+	{
+		return this.studentID;
+	}
+	
+	public int[] getScores()
+	{
+		return this.scores;
+	}
 }
